92b99f79e53b460669ef5b775ddb5100ef6d3f2b
Alan Schneider <alan@aschneider.com>
2020-04-22 17:57:27 -0700

    Made some more Terraform updates based on their two-tier example. Needs to be reworked for the weather forecast example

---
+++ infrastructure/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_ami" {
+  type = string
+}
+
+variable "aws_key_name" {
+  type = string
+}
+
+variable "aws_public_key" {
+  type = string
+}
+
+variable "aws_instance_type" {
+  type = string
+}
