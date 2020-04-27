f772bf76ec6bc122b1b9e385ce278df094f916bd
Alan Schneider <alan@aschneider.com>
2020-04-26 18:51:55 -0700

    Need to pass the key pair into the instance config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -135,6 +135,9 @@ resource "aws_instance" "web" {
   instance_type = var.aws_instance_type
   ami           = var.aws_ami
 
+  # The name of our SSH keypair we created above.
+  key_name = aws_key_pair.auth.id
+
   # Our Security group to allow HTTP and SSH access
   vpc_security_group_ids = [ aws_security_group.default.id ]
 
